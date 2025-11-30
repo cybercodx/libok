39122a479bb1505d6cdfc16a656a0e5d65639681
cybercodx <ganoshasthyakendra@gmail.com>
2025-11-30 21:26:40 +0600

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,19 +1,33 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import doShardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-//import { withFilter, softTagFilter } from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
+import { purgeCache } from "@opennextjs/cloudflare/overrides/cache-purge/index";
  
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: "long-lived",
+    // Setting `bypassTagCacheOnCacheHit` to `true` requires enabling cache purge
+    bypassTagCacheOnCacheHit: true,
+  }),
   queue: doQueue,
   // This is only required if you use On-demand revalidation
-  tagCache: d1NextTagCache,
-  //If you don't use `revalidatePath`, you can also filter internal soft tags using the `softTagFilter`
-  // tagCache: withFilter({
-  //   tagCache: d1NextTagCache,
-  //   filterFn: softTagFilter,
-  // }),
+  tagCache: doShardedTagCache({
+    baseShardSize: 12,
+    regionalCache: true, // Enable regional cache to reduce the load on the DOs
+    regionalCacheTtlSec: 5, // The TTL for the regional cache
+    regionalCacheDangerouslyPersistMissingTags: true, // Enable this to persist missing tags in the regional cache
+    shardReplication: {
+      numberOfSoftReplicas: 4,
+      numberOfHardReplicas: 2,
+      regionalReplication: {
+        defaultRegion: "enam",
+      },
+    },
+  }),
   // Disable this if you want to use PPR
   enableCacheInterception: true,
+  // you can also use the `durableObject` option to use a durable object as a cache purge
+  cachePurge: purgeCache({ type: "direct" }),
 });
